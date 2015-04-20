37111c8b6b059a7b1b518ce59c6534a7e8e6d9da
Ayman Osman <aymano.osman@gmail.com>
2015-04-20 23:11:34 +0100

    wip

--- logfmt.hs
+++ logfmt.hs
@@ -1,8 +1,20 @@
 module Logfmt where
 
+import Control.Applicative
 import Data.Attoparsec.ByteString as P
-import Data.ByteString.Char8 as C
+import Data.Attoparsec.ByteString.Char8 as C
+import qualified Data.ByteString.Char8 as K
 
-p = P.takeWhile (P.notInClass "=\"")
+p1 = P.takeWhile (P.notInClass "=\"")
+p2 = p1 <* char8 '='
 
-main = parse p (C.pack "key=\"value\"")
+-- main = P.parse p (K.pack "key=\"value\"")
+
+test = do
+  t p1 s (Right "key")
+  t p2 s (Right "=")
+  where
+    s = "key=\"value\""
+    t p s res = case P.parse p (K.pack s) of
+      res -> putStrLn "Success :)"
+      _ -> putStrLn "Fail :("
