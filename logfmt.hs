bd43a3a8eff4a0920c292e72bfe601c731429f2a
Ayman Osman <aymano.osman@gmail.com>
2015-05-03 12:11:45 +0100

    Nearly there

--- logfmt.hs
+++ logfmt.hs
@@ -4,52 +4,57 @@ module Logfmt where
 
 import Control.Monad
 import Control.Applicative
+import qualified Data.ByteString as B
 import Data.Attoparsec.ByteString as P
 import Data.Attoparsec.ByteString.Char8 as C
 import qualified Data.ByteString.Char8 as K
 
-
-p1 = P.takeWhile (P.notInClass "=\"")
-p2 = p1 <* char8 '='
-
 data Pair = Pair String String deriving (Show)
 
 parsePair :: Parser Pair
-parsePair = liftA2 Pair (fmap K.unpack p2) (fmap K.unpack parseVal)
-
-parseVal = P.takeWhile (P.notInClass " ")
-
-p3 = (parsePair <* ws) <* parsePair
+parsePair = do
+  key <- K.unpack <$> (ws *> pKey)
+  char '='
+  val <- K.unpack <$> pVal
+  return $ Pair key val
+  where
+    pKey = P.takeWhile (P.notInClass "=")
+    pVal = do
+      Just c1 <- C.peekChar
+      val <- if c1 == '"'
+             then parseQuoted
+             else parseUnquoted
+      return val
+    parseUnquoted :: Parser B.ByteString
+    parseUnquoted = P.takeWhile (P.notInClass " ")
+    parseQuoted :: Parser K.ByteString
+    parseQuoted = do
+      char '"'
+      v <- P.takeWhile (P.notInClass "\"") <* char '"'
+      return v
 
 ws = P.takeWhile (P.inClass " ")
 
-finalP :: Parser [Pair]
-finalP = do
-  p1 <- many (parsePair <* skipWhitespace)
-  return $ p1
-  where
-    skipWhitespace = ws
+parseLine :: Parser [Pair]
+parseLine = (many parsePair) <* endOfLine
 
-s = "key=value foo=\"a bar\""
-s2 = "key=\"value\" foo=bar"
-s3 = "key=\"a value\" foo=bar"
-s4 = "a=1 b=2 c=3"
+s = "key=value foo=\"a bar\"\n"
+s2 = "key=\"value\" foo=bar\n"
+s3 = "key=\"a value\" foo=bar\n"
+s4 = "a=1 b=2 c=3\n"
 
 main = do
-  -- p p1 s
-  -- p p2 s
-  -- p parsePair s
-  -- p parsePair s3
-  -- p p3 s
-  p finalP s4
-  p finalP s3
+  p parseLine s
+  p parseLine s2
+  p parseLine s3
+  p parseLine s4
   where
     p pr s = do
       putStrLn (K.unpack s)
-      case (parse pr s) of
-        Done i r -> do
+      case (parseOnly pr s) of
+        Right r -> do
           print r
-          putStrLn (K.unpack i)
+          -- putStrLn (K.unpack i)
         x -> do
           print "FAILURE"
           print x
