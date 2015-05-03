3848c1374529e8999692faecf0a66783d733b0db
Ayman Osman <aymano.osman@gmail.com>
2015-05-03 19:47:47 +0100

    wip

--- logfmt.hs
+++ logfmt.hs
@@ -42,15 +42,19 @@ s4 = "a=1 b=2 c=3"
 s5 = "schmoo=\"hello\nworld\""
 s6 = "ip=127.0.0.1 time=2015-05-03T13:54:03.396Z method=GET path=\"/test/user/has-friend?friend=example\" status=304 content_length= content_type= elapsed=2ms"
 s7 = "a=foo b=10ms c=cat E=\"123\" d foo= emp="
+s8 = "measure.a=1ms measure.b=10 measure.c=100MB measure.d=1s garbage"
+s9 = "foo=bar a=14 baz=\"hello kitty\" cool%story=bro f %^asdf"
+
+ss = [s6, s7, s8, s9]
 
 main = do
-  p parseLine s6
-  p parseLine s7
+  forM_ ss (p parseLine)
   where
   p pr s = do
     K.putStrLn s
     let Right r = parseOnly pr s
     mapM_ print r
+    putStrLn ""
 
 main2 = do
   p parseLine s
