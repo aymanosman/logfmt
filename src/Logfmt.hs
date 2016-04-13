410b3de16ac796399c58d446599bb0c9d642de7d
Ayman Osman <aymano.osman@gmail.com>
2016-04-13 18:03:13 +0100

    tidy

--- src/Logfmt.hs
+++ src/Logfmt.hs
@@ -4,20 +4,23 @@ module Logfmt where
 import Control.Applicative
 import Data.Attoparsec.Text (Parser)
 import qualified Data.Attoparsec.Text as P
-import Data.Monoid
 import Data.Text (Text)
 
 parseLine :: Parser [(Text, Maybe Text)]
 parseLine =  many (P.skipSpace *> parsePair)
+
+parsePair :: Parser (Text, Maybe Text)
 parsePair = (,) <$> parseKey <*> parseValue
 
+parseKey :: Parser Text
 parseKey = P.takeWhile1 (P.notInClass "=\" ")
 
-parseValue = do
-  c <- P.peekChar
-  case c of
-    Just '=' -> P.char '=' >> val
-    _ -> return Nothing
+parseValue :: Parser (Maybe Text)
+parseValue =
+  do c <- P.peekChar
+     case c of
+       Just '=' -> P.char '=' >> val
+       _ -> return Nothing
   where
     val = do
       c <- P.peekChar
@@ -26,9 +29,12 @@ parseValue = do
         Just '"' -> parseQuoted
         _ -> parseUnquoted
 
+parseUnquoted :: Parser (Maybe Text)
 parseUnquoted = Just <$> P.takeWhile (P.notInClass " \"")
+
+parseQuoted :: Parser (Maybe Text)
 parseQuoted = P.char '"' *> s <* P.char '"'
   where
-    s = Just <$> mconcat <$> many (esc <|> stringBytes)
+    s = Just . mconcat <$> many (esc <|> stringBytes)
     esc = P.string "\\\""
     stringBytes = P.takeWhile1 (P.notInClass "\"\\")
