ab4784f9752484516d162d3c21f355167afa997b
Ayman Osman <aymano.osman@gmail.com>
2016-04-13 17:45:12 +0100

    make to work with stack

--- README.md
+++
@@ -1,9 +0,0 @@
-```js
-l.parse('a= emp"a \\\"value"')                                                                       │ 27     let s = "a=foo b=10ms c=cat E=\"123\" d foo= emp="
-// { a: null, 'empa value': true }
-```
-
-```python
-l.parse_line('a= emp="a \\\"value"')
-# {'a': True, 'emp': 'a "value'}
-```
--- Spec.hs
+++ Spec.hs
@@ -2,68 +2,68 @@
 
 import Test.Hspec
 import Test.Hspec.Attoparsec
-import Test.QuickCheck
-import Control.Exception (evaluate)
+-- import Test.QuickCheck
 import Data.Monoid
 import Data.Text (Text, pack, unpack, isInfixOf)
 
 import Logfmt
 
-type T = Text
-
+encode' :: [(Text, Maybe Text)] -> Text
 encode' [] = ""
 encode' ((a,b):xs) = a <> conv b <> " " <> encode' xs
   where
-    conv b = case b of
-      Just s -> "=" <> f s
-      Nothing -> ""
-    f s = if " " `isInfixOf` s
-          then pack $ show s else s
+    conv x =
+      case x of
+        Just s -> "=" <> f s
+        Nothing -> ""
+    f s =
+      if " " `isInfixOf` s
+      then pack $ show s else s
 
-main = hspec $ do
+main :: IO ()
+main = hspec $
   describe "Examples" $ do
 
     let exp1 = [("key", Just "value") , ("foo", Just "a bar")]
-    let s = encode' exp1
-    it (unpack s) $ do
-      (s :: T) ~> parseLine `shouldParse` exp1
+    let s1 = encode' exp1
+    it (unpack s1) $
+      (s1 :: Text) ~> parseLine `shouldParse` exp1
 
-    let s = "\""
-    it (unpack s) $ do
-      (s :: T) ~> parseLine `shouldParse` []
+    let s2 = "\""
+    it (unpack s2) $
+      (s2 :: Text) ~> parseLine `shouldParse` []
 
-    let s = "a=foo b=10ms c=cat E=\"123\" d foo= emp="
-    it (unpack s) $ do
-      (s :: T) ~> parseLine
-        `shouldParse` [
-         ("a", Just "foo")
-        , ("b", Just "10ms")
-        , ("c", Just "cat")
-        , ("E", Just "123")
-        , ("d", Nothing)
-        , ("foo", Just "")
-        , ("emp", Just "")
-        ]
+    let s3 = "a=foo b=10ms c=cat E=\"123\" d foo= emp="
+    it (unpack s3) $
+      (s3 :: Text) ~> parseLine `shouldParse`
+      [ ("a", Just "foo")
+      , ("b", Just "10ms")
+      , ("c", Just "cat")
+      , ("E", Just "123")
+      , ("d", Nothing)
+      , ("foo", Just "")
+      , ("emp", Just "")
+      ]
 
-    let s = "a=2 b=\"a str\\\"ing\" f"
-    it (unpack s) $ do
-      (s  :: T) ~> parseLine `shouldParse` [
-        ("a", Just "2")
-        , ("b", Just "a str\\\"ing")
-        , ("f", Nothing)
-        ]
+    let s4 = "a=2 b=\"a str\\\"ing\" f"
+    it (unpack s4) $
+      (s4  :: Text) ~> parseLine `shouldParse`
+      [ ("a", Just "2")
+      , ("b", Just "a str\\\"ing")
+      , ("f", Nothing)
+      ]
 
-    let s = "f a=\" hello\\\"world\\\"\""
-    it (unpack s) $ do
-      (s :: T) ~> parseLine `shouldParse` [
-        ("f", Nothing)
-        , ("a", Just " hello\\\"world\\\"")
-        ]
+    let s5 = "f a=\" hello\\\"world\\\"\""
+    it (unpack s5) $
+      (s5 :: Text) ~> parseLine `shouldParse`
+      [ ("f", Nothing)
+      , ("a", Just " hello\\\"world\\\"")
+      ]
 
-    let s = "a=2 b=abc\"de f"
-    it (unpack s) $ do
-      (s::T) ~> parseLine `shouldParse` [
-        ("a", Just "2")
-        , ("b", Just "abc")
-        ]
+    let s6 = "a=2 b=abc\"de f"
+    it (unpack s6) $
+      (s6::Text) ~> parseLine `shouldParse`
+      [ ("a", Just "2")
+      , ("b", Just "abc")
+      ]
 
